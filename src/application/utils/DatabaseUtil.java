3fa5a370aa7eb39e087f6143b8cfad66b0da8298
Yusuf Eko Anggoro <anggoroekoyusuf@gmail.com>
2024-08-13 15:43:03 +0700

    refactor code

--- src/application/utils/DatabaseUtil.java
+++ src/application/utils/DatabaseUtil.java
@@ -18,7 +18,7 @@ public class DatabaseUtil {
 
     private static Connection connection;
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String URL = "jdbc:mysql://localhost:3306/codebase";
+    private static final String URL = "jdbc:mysql://localhost/ahp_recruit";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "";
     private static final String MAX_POOL = "250";
